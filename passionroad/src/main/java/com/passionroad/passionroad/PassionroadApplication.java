fee04d1ad185d0976ffa977ea0938b6638ac76b5
caco <ziczin7176@gmail.com>
2023-03-10 15:36:03 +0900

    [Feat]: 스프링 프로젝트 생성

---
+++ passionroad/src/main/java/com/passionroad/passionroad/PassionroadApplication.java
@@ -0,0 +1,13 @@
+package com.passionroad.passionroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassionroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassionroadApplication.class, args);
+	}
+
+}
---
+++ passionroad/src/test/java/com/passionroad/passionroad/PassionroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passionroad.passionroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassionroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
