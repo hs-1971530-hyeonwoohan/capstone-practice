81229767a333a722b4c0dad44107bb7681edd6a0
woo <zzx_00@naver.com>
2023-03-22 16:10:55 +0900

    [feat] : 로그인 관련 작업 진행 및 페이지 추가 현재 로그인 관련 기능이 완전하게 작동하는 것은 아니나, 몇 가지 코드 상의 추가 사항과 관련 코드들을 함께 커밋합니다. 로그인 폼 페이지로의 Router도 설정 되어 있습니다.

--- FE/main/src/pages/dashboard/DashBoard.jsx
+++ FE/main/src/pages/dashboard/DashBoard.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import DashLeft from "../../components/dashLeft/DashLeft";
+import DashRight from "../../components/dashRight/DashRight";
+import NavBar from "../../components/navbar/NavBar";
+import { ActiveTapState } from "../../atoms/ActiveTapState";
+import { useRecoilState } from "recoil";
 
 function DashBoard() {
+  const [activeNav, setActiveNav] = useRecoilState(ActiveTapState);
+
   return (
     <div>
-      <div>
-        <Header />
-      </div>
-      <div className="w-full min-h-[90vh] grid grid-cols-12">
+      <div className="w-full min-h-[h-screen] grid grid-cols-12">
         <NavBar />
         <div className="grid grid-cols-1 xl:grid-cols-5 w-full col-span-10">
           <DashLeft />
