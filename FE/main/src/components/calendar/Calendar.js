ab6f6cd860eb9f35a9df4d203bd188f876d872d6
woo <zzx_00@naver.com>
2023-03-22 16:14:47 +0900

    [feat] : 캘린더 페이지 추가 및 라우터 지정 3/22 일자 커밋은 여기까지 pull 하셔야 정상적으로 렌더링이 될 것 같습니다. 캘린더 관련 페이지와 Header에서의 링킹과 App.js에서의 라우팅 지정이 있습니다.

---
+++ FE/main/src/components/calendar/Calendar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import '@fullcalendar/common/main.css'; // 수정된 경로
+
+function Calendar() {
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('이벤트 제목을 입력하세요:');
+    let calendarApi = selectInfo.view.calendar;
+  
+    calendarApi.unselect(); // 선택한 날짜 선택 해제
+  
+    if (title) {
+      calendarApi.addEvent({
+        title: title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };  
+
+  return (
+    <div className='pt-5 px-20'>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+        initialView='dayGridMonth'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+        }}
+        events={[
+          { title: 'event 1', date: '2023-03-18' },
+          { title: 'event 2', date: '2023-03-19' },
+        ]}
+        selectable={true} // 드래그 선택 가능
+        select={handleDateSelect} // 선택한 날짜와 시간 값 출력
+        dayMaxEventRows={true} // 일정이 많아졌을 때 버튼으로 펼쳐서 볼 수 있게 함
+        views={{
+          dayGridMonth: {
+            dayMaxEventRows: 2, // 2줄까지만 보이도록 설정
+            moreLinkText: 'more', // 더보기 버튼 텍스트 설정
+          },
+        }}
+
+
+      />
+    </div>
+  );
+}
+
+export default Calendar;
