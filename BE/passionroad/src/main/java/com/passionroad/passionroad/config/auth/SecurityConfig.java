ea7fc3710c21627de378f2bb9fb6c356bcea727c
hyeonwoo <ziczin7176@gmail.com>
2023-04-07 14:17:45 +0900

    [Fix]: security client 버전변경에 따른 설정 변경

--- BE/passionroad/src/main/java/com/passionroad/passionroad/config/auth/SecurityConfig.java
+++ BE/passionroad/src/main/java/com/passionroad/passionroad/config/auth/SecurityConfig.java
@@ -2,17 +2,44 @@ package com.passionroad.passionroad.config.auth;
 
 import com.passionroad.passionroad.domain.user.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig{
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    @Override
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**", "/css/**", "/images/**", "/js/**", "/resources/**", "/profile", "/favicon.ico").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())    // .hasRole(Role.USER.name()
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+
+        return http.build();
+    }
+
+
+
+
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
@@ -29,5 +56,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
-    }
+    }*/
 }
