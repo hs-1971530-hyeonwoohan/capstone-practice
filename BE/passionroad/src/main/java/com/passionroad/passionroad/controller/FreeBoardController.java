0f88abef6c9c2d2a952a3a73e741ca8989672f92
hyeonwoo <ziczin7176@gmail.com>
2023-04-11 04:39:14 +0900

    [Feat]: 특정 게시글 조회 api 메소드 작성

--- BE/passionroad/src/main/java/com/passionroad/passionroad/controller/FreeBoardController.java
+++ BE/passionroad/src/main/java/com/passionroad/passionroad/controller/FreeBoardController.java
@@ -3,10 +3,12 @@ package com.passionroad.passionroad.controller;
 import com.passionroad.passionroad.dto.FreeBoardDTO;
 import com.passionroad.passionroad.dto.PageRequestDTO;
 import com.passionroad.passionroad.dto.PageResponseDTO;
+import com.passionroad.passionroad.repository.FreeBoardRepository;
 import com.passionroad.passionroad.repository.UserRepository;
 import com.passionroad.passionroad.service.FreeBoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -37,14 +39,23 @@ public class FreeBoardController {
     private final UserRepository userRepository;
 
     // write freeboard post
-//    @PostMapping
-//    public ResponseEntity<FreeBoardDTO> writePost(@RequestBody @Valid FreeBoardDTO freeBoardDTO, @AuthenticationPrincipal UserDetails userDetails){
-//
-//        return null;
-//    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<FreeBoardDTO> writePost(
+            @RequestBody @Valid FreeBoardDTO freeBoardDTO,
+            @AuthenticationPrincipal UserDetails userDetails){
+
+        // get user info
+//        userDetails.getUsername()
+
+        return null;
+    }
 
     @GetMapping
-    public ResponseEntity<PageResponseDTO<FreeBoardDTO>> listPosts(@RequestParam(defaultValue = "1") String page, @RequestParam(defaultValue = "10") String size, @RequestParam(required = false) String type, @RequestParam(required = false) String keyword) {
+    public ResponseEntity<PageResponseDTO<FreeBoardDTO>> listPosts(
+            @RequestParam(defaultValue = "1") String page,
+            @RequestParam(defaultValue = "10") String size,
+            @RequestParam(required = false) String type,
+            @RequestParam(required = false) String keyword) {
 
         // make page request dto with url parameter
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
@@ -59,4 +70,14 @@ public class FreeBoardController {
 
         return ResponseEntity.ok(pageResponseDTO);
     }
+
+    // read a specific post
+    @GetMapping("/{id}")
+    public ResponseEntity<FreeBoardDTO> readPost(@PathVariable Long id){
+
+        FreeBoardDTO freeBoardDTO = freeBoardService.readOne(id);
+
+        return ResponseEntity.ok(freeBoardDTO);
+    }
+
 }
