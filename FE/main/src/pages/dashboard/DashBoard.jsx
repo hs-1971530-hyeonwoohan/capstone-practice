dc60664b893edf1c993bee9bfd9b116b2cefe79b
woo <zzx_00@naver.com>
2023-05-03 15:05:41 +0900

    [Feat] 대쉬보드 열정도 애니메이션 및 navBar 인덱스 수정. 애니메이션은 tailwind.config에 추가되었으며   DashLeft에서 적용됩니다. NavBar의 index버그를 수정하였습니다. ActiveTapState.js를 통해 버튼 인덱스의 상태관리가 이루어집니다. BenefitModal은 열정도 혜택에 대한 사항을 알려주기 위한 모달입니다. 해당 모달은 아직 구현이 덜 된 상태에 있습니다.

--- FE/main/src/pages/dashboard/DashBoard.jsx
+++ FE/main/src/pages/dashboard/DashBoard.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DashLeft from "../../components/dashLeft/DashLeft";
 import DashRight from "../../components/dashRight/DashRight";
 import NavBar from "../../components/navbar/NavBar";
 import { ActiveTapState } from "../../atoms/ActiveTapState";
 import { useRecoilState } from "recoil";
+import Profile from "../profile/Profile";
+
 
 function DashBoard() {
   const [activeNav, setActiveNav] = useRecoilState(ActiveTapState);
 
+  useEffect(() => {
+    // 상태가 변경될 때마다 실행할 부수 효과를 여기에 작성합니다.
+    // 예: activeNav 상태에 따라 특정 작업을 수행하거나, API 호출 등을 할 수 있습니다.
+
+    console.log('현재 activeNav:', activeNav);
+    setActiveNav(0);
+  }, []);
+
   return (
     <div>
-      <div className="w-full min-h-[h-screen] grid grid-cols-12">
+      <div className="w-full min-h-[h-screen] flex">
         <NavBar />
-        <div className="grid grid-cols-1 xl:grid-cols-5 w-full col-span-10">
-          <DashLeft />
-          <DashRight />
+
+        <div>
+          {activeNav === 0 && (
+            <div className="grid grid-cols-1 xl:grid-cols-5 w-full col-span-10">
+              <DashLeft />
+              <DashRight />
+            </div>
+          )}
+
+          {activeNav === 5 && (<div className="grid grid-cols-1 xl:grid-cols-5 w-full col-span-10l">
+              <div className="xl:col-span-1"></div> {/* 왼쪽 여백을 위한 빈 div */}
+              <div className="px-10 xl:col-span-4">
+                <Profile />
+              </div>
+            </div>)}
         </div>
       </div>
     </div>
