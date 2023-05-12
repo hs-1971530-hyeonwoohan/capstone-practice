3e7e9970aa6184073fd54d69755b3fedf7ec2062
Taeyeong Kim <tyeong1102@gmail.com>
2023-05-12 15:50:18 +0900

    [Feat] 스터디룸 Request 추가

---
+++ BE/passionroad/src/main/java/com/passionroad/passionroad/studyroom/request/StudyRoomEnterRequestDto.java
@@ -0,0 +1,10 @@
+package com.passionroad.passionroad.studyroom.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudyRoomEnterRequestDto {
+    private String roomId;
+}
