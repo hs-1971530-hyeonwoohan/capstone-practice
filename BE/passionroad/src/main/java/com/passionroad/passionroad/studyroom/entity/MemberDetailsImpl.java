5fc3685ce082657de84cc6c7796ca9428ed604a1
Taeyeong Kim <tyeong1102@gmail.com>
2023-05-12 16:05:14 +0900

    [Feat] MemberDetailsImpl 추가

--- BE/passionroad/src/main/java/com/passionroad/passionroad/studyroom/entity/MemberDetailsImpl.java
+++ BE/passionroad/src/main/java/com/passionroad/passionroad/studyroom/entity/MemberDetailsImpl.java
@@ -1,7 +1,11 @@
 package com.passionroad.passionroad.studyroom.entity;
 
 import com.passionroad.passionroad.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class MemberDetailsImpl implements UserDetails {
 
@@ -16,8 +20,38 @@ public class MemberDetailsImpl implements UserDetails {
     }
 
     @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
     public String getUsername() {
         return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
 }
