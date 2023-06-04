49131e61a5054b37cb513535a62a90d499ae9cfb
hyeonwoo <53888037+hyeonwoo-han@users.noreply.github.com>
2023-06-05 05:09:35 +0900

    Revert "Wkdlrn"

--- FE/main/src/components/dashLeft/InvestItem.jsx
+++ FE/main/src/components/dashLeft/InvestItem.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 function InvestItem({ item }) {
-  const { title, desc, icon, image, percent, price } = item; //destructioring
+  const { title, desc, icon, upOrDown, percent, price } = item; //destructioring
   return (
     <div className="w-full py-2 px-2 items-cener justify-between flex">
       {/* icon + text */}
       <div className="flex items-center justify-center space-x-4 w-full  ">
-        {/* <div className="bg-[#DBEEF4] rounded-full py-2 px-2"> */}
-        <div>
-        <img src={image} className="w-[300px] rounded-lg"/>
+        <div className="bg-[#DBEEF4] rounded-full py-2 px-2">
+          <span>{icon}</span>
         </div>
         <div className="w-full space-y-1">
-          <h4 className="font-bold text-xl">{title}</h4>
+          <h4 className="font-bold">{title}</h4>
           <p className="text-sm">{desc}</p>
         </div>
       </div>
--- FE/main/src/components/verticalslider/VerticalDropdown.jsx
+++ FE/main/src/components/verticalslider/VerticalDropdown.jsx
@@ -1,46 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 
 export default function VerticalDropdown() {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState("하루 전");
-
   const userNavigation = [
     { name: "하루 전" },
     { name: "최근 1주일" },
     { name: "최근 1개월" },
   ];
 
-  const handleItemClick = (name) => {
-    setSelectedItem(name);
-    setIsOpen(false);
-  };
-
   return (
     <div className="relative flex-col">
-      <div className="flex">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
         className=" flex justify-items-start items-center  font-bold text-2xl text-teal-500 tracking-wider mb-2"
       >
-        {selectedItem}
+        하루 전
         {!isOpen ? (
           <AiOutlineCaretDown className="h-8" />
         ) : (
           <AiOutlineCaretUp className="h-8" />
         )}
-        </button>
-        <p className="justify-items-start items-center  font-bold text-2xl text-black tracking-wider mb-2 ml-4">누적 공부 시간 랭킹</p>
-        </div>
+        <p className="font-bold text-black ml-4">누적 공부 시간 랭킹</p>
+      </button>
 
       {isOpen && (
         <div className=" bg-white absolute z-20  flex flex-col items-start rounded-lg p-2 w-full">
           {userNavigation.map((item) => (
-            <div
-              key={item.name}
-              onClick={() => handleItemClick(item.name)}
-              className="flex w-full justifiy-between hover:bg-gray-300 cursor-pointer rounded-r-lg"
-            >
+            <div className="flex w-full justifiy-between hover:bg-gray-300 cursor-pointer rounded-r-lg">
               <h3 className="font-bold">{item.name}</h3>
             </div>
           ))}
